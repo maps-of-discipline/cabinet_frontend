5387049352b37610b0ec7090b2a76f244fe39e20
Daniil Kirillov <chunaixxx@gmail.com>
2024-03-20 02:23:15 +0300

    Добавлен RolesEnum

---
+++ src/models/auth/RolesEnum.ts
@@ -0,0 +1,10 @@
+enum RoleNamesEnum {
+	Student = 'Студент',
+	Teacher = 'Куратор',
+	Tutor = 'Тьютор',
+	Faculty = 'Администратор',
+	Curator = 'Куратор',
+	Admin = 'Администратор',
+}
+
+export { RoleNamesEnum }
